6c0e5e796a3698cdf218c1c185acac5eb698d15e
xsavier69 <pabloxaseg@gmail.com>
2024-03-29 21:32:02 -0500

    v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,44 +7,53 @@ import { Database, object, ref } from '@angular/fire/database';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  valorSala: boolean = false;
-  valorCocina: boolean = false;
-  valorComedor: boolean = false;
-  valorDormitorio1: boolean = false;
-  valorDormitorio2: boolean = false;
-  valorBano: boolean = false;
-
-  constructor(private database: Database) {}
-
-  ngOnInit() {
-    const routeSala = ref(this.database, "/casa/sala");
-    object(routeSala).subscribe(attributes => {
-      this.valorSala = attributes.snapshot.val();
-    });
-
-    const routeCocina = ref(this.database, "/casa/cocina");
-    object(routeCocina).subscribe(attributes => {
-      this.valorCocina = attributes.snapshot.val();
-    });
-
-    const routeComedor = ref(this.database, "/casa/comedor");
-    object(routeComedor).subscribe(attributes => {
-      this.valorComedor = attributes.snapshot.val();
-    });
-
-    const routeDormitorio1 = ref(this.database, "/casa/dormitorio1");
-    object(routeDormitorio1).subscribe(attributes => {
-      this.valorDormitorio1 = attributes.snapshot.val();
-    });
-
-    const routeDormitorio2 = ref(this.database, "/casa/dormitorio2");
-    object(routeDormitorio2).subscribe(attributes => {
-      this.valorDormitorio2 = attributes.snapshot.val();
-    });
-
-    const routeBano = ref(this.database, "/casa/bano");
-    object(routeBano).subscribe(attributes => {
-      this.valorBano = attributes.snapshot.val();
-    });
-  }
-}
+   // Declaración e inicialización de variables booleanas para representar el estado de diferentes partes de la casa
+   valorSala: boolean = false;
+   valorCocina: boolean = false;
+   valorComedor: boolean = false;
+   valorDormitorio1: boolean = false;
+   valorDormitorio2: boolean = false;
+   valorBano: boolean = false;
+ 
+   // Constructor del componente HomePage que recibe el servicio de base de datos como parámetro
+   constructor(private database: Database) {}
+ 
+   // Método ngOnInit que se ejecuta al inicializar el componente
+   ngOnInit() {
+     // Suscripción a la ruta "/casa/sala" en la base de datos para obtener el valor booleano asociado a la sala
+     const routeSala = ref(this.database, "/casa/sala");
+     object(routeSala).subscribe(attributes => {
+       this.valorSala = attributes.snapshot.val();
+     });
+ 
+     // Suscripción a la ruta "/casa/cocina" en la base de datos para obtener el valor booleano asociado a la cocina
+     const routeCocina = ref(this.database, "/casa/cocina");
+     object(routeCocina).subscribe(attributes => {
+       this.valorCocina = attributes.snapshot.val();
+     });
+ 
+     // Suscripción a la ruta "/casa/comedor" en la base de datos para obtener el valor booleano asociado al comedor
+     const routeComedor = ref(this.database, "/casa/comedor");
+     object(routeComedor).subscribe(attributes => {
+       this.valorComedor = attributes.snapshot.val();
+     });
+ 
+     // Suscripción a la ruta "/casa/dormitorio1" en la base de datos para obtener el valor booleano asociado al dormitorio 1
+     const routeDormitorio1 = ref(this.database, "/casa/dormitorio1");
+     object(routeDormitorio1).subscribe(attributes => {
+       this.valorDormitorio1 = attributes.snapshot.val();
+     });
+ 
+     // Suscripción a la ruta "/casa/dormitorio2" en la base de datos para obtener el valor booleano asociado al dormitorio 2
+     const routeDormitorio2 = ref(this.database, "/casa/dormitorio2");
+     object(routeDormitorio2).subscribe(attributes => {
+       this.valorDormitorio2 = attributes.snapshot.val();
+     });
+ 
+     // Suscripción a la ruta "/casa/bano" en la base de datos para obtener el valor booleano asociado al baño
+     const routeBano = ref(this.database, "/casa/bano");
+     object(routeBano).subscribe(attributes => {
+       this.valorBano = attributes.snapshot.val();
+     });
+   }
+ }
